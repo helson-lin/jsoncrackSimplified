e8c5923108faf37d86b4f7283498e1c6235cb846
lin <>
2024-07-25 16:01:30 +0800

    update: update static files

--- out/_next/static/chunks/86479803-e9b4306a384e9a3c.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6478],{54678:function(t,a,n){n.d(a,{IPg:function(){return l}});var r=n(90989);function l(t){return(0,r.w_)({tag:"svg",attr:{fill:"currentColor",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{d:"M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0"},child:[]}]})(t)}}}]);
-//# sourceMappingURL=86479803-e9b4306a384e9a3c.js.map
